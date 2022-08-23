ee1feab59cc11762f12bddbb30674f8580118e53
Irina <irishavoronkina@gmail.com>
2022-08-23 18:12:52 +0300

    Read from file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 #include <vector>
 
-template<typename T>
-bool IsPrime(T num) {
-  for (T i = 2; i < static_cast<T>(std::sqrt(num) + 1); i++) {
+bool IsPrime(uint64_t num) {
+  for (uint64_t i = 2; i < (std::sqrt(num) + 1); i++) {
     if (num % i == 0) {
       return false;
     }
@@ -13,10 +13,9 @@ bool IsPrime(T num) {
   return true;
 }
 
-template<typename T>
-std::pair<bool, std::vector<int>> GetNumbers(T num) {
+std::pair<bool, std::vector<int8_t>> GetNumbers(uint64_t num) {
   bool out = true;
-  std::vector<int> numbers;
+  std::vector<int8_t> numbers;
   while (num != 0) {
     int t = num % 10;
     if (t == 5 || t % 2 == 0) {
@@ -26,67 +25,56 @@ std::pair<bool, std::vector<int>> GetNumbers(T num) {
     numbers.push_back(t);
     num /= 10;
   }
-  int sum = 0;
-  for (int number: numbers) {
-    sum += number;
-  }
-  if (sum % 3 == 0) {
-    out = false;
-  }
   return std::make_pair(out, numbers);
 }
 
-template<typename T>
-std::pair<bool, std::vector<T>> WringNumber(std::vector<int> numbers) {
-  bool out = true;
-  std::vector<T> returning;
-  std::sort(numbers.begin(), numbers.end());
-  do {
-    T number = 0;
-    for (int i = 0; i < numbers.size() - 1; i++) {
-      number += numbers[i];
-      number *= 10;
+bool BinFind(const std::vector<uint64_t>& numbers, uint64_t value) {
+  int midd = 0;
+  int left = 0;
+  int right = numbers.size();
+  while (left <= right) {
+    midd = (left + right) / 2;
+
+    if (value < numbers[midd])
+      right = midd - 1;
+    else if (value > numbers[midd])
+      left = midd + 1;
+    else
+      return true;
+  }
+  return false;
+}
+
+void WringNumber(const std::vector<uint64_t>& numbers) {
+  for (auto number: numbers) {
+    if (!GetNumbers(number).first) break;
+
+    bool is_beautiful = true;
+    std::string s{std::to_string(number)};
+    std::sort(s.begin(), s.end());
+    do {
+      unsigned long value{std::stoul(s)};
+      if (!BinFind(numbers, value)) {
+        is_beautiful = false;
+        break;
+      }
+    } while (std::next_permutation(s.begin(), s.end()));
+    if (is_beautiful) {
+      std::cout << number << '\n';
     }
-    number += numbers.back();
-    if (!IsPrime(number)) {
-      out = false;
-      break;
-    }
-    returning.push_back(number);
-  } while (std::next_permutation(numbers.begin(), numbers.end())); // смущает
-  // эта функция (мб надо не ее)
-  return std::make_pair(out, returning);
+  }
 }
 
 int main() {
+  // todo
 
-  std::vector<int> beautiful_primes; // в перспективе - сделать еще для
-  // unsigne int, ..., unsigned long long. Для этого в функциях стоят template-ы
-  for (int i = 0; i < 10'000'000; i++) {
-    if (IsPrime(i)) {
-      std::pair<bool, std::vector<int>> pair = GetNumbers(i);
-      if (pair.first) {
-        std::pair<bool, std::vector<int>> pair2 = WringNumber<int>(pair.second);
-        if (pair2.first) {
-          beautiful_primes.push_back(i);
-        }
-      }
-    }
+  std::vector<uint64_t> primes;
+  std::fstream fin;
+  uint64_t prime;
+  while (!fin.eof()) {
+    std::cin >> prime;
+    primes.emplace_back(prime);
   }
-
-  while (!beautiful_primes.empty()) {
-    std::vector<int>
-        nums = WringNumber<int>(GetNumbers(beautiful_primes[0]).second).second;
-    for (int& num: nums) {
-      auto it = std::find(beautiful_primes.begin(),
-                          beautiful_primes.end(),
-                          num);
-      if (it != beautiful_primes.end()) {
-        std::cout << *it << " ";
-        beautiful_primes.erase(it);
-      }
-    }
-    std::cout << "\n";
-  }
+  WringNumber(primes);
   return 0;
 }
